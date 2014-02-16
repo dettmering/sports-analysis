5dfe2928dd5db929715fee9a334fc3709164545f
Till Dettmering <prinzipiell@gmail.com>
2014-02-16 11:17:43 +0100

    Added several standard plots

--- figures.R
+++ figures.R
@@ -11,9 +11,23 @@ ggplot(a, aes(x = x, y = y)) +
   coord_equal() +
   theme_bw()
 
+ggplot(a, aes(x = Cumulative.Distance.m, y = Elevation.m)) +
+  geom_area(alpha = 0.8) +
+  xlab("Distance (m)") +
+  ylab("Elevation (m)") +
+  theme_bw()
+
+ggplot(a, aes(x = Cumulative.Distance.m, y = Speed.kmh)) +
+  geom_line() +
+  annotate("rect", xmin = 0, xmax = max(a$Cumulative.Distance.m, na.rm = T), ymin = mean(a$Speed.kmh, na.rm = TRUE) - sd(a$Speed.kmh, na.rm = TRUE), ymax = mean(a$Speed.kmh, na.rm = TRUE) + sd(a$Speed.kmh, na.rm = TRUE), alpha = .2) +
+  annotate("segment", y = mean(a$Speed.kmh, na.rm = TRUE), yend = mean(a$Speed.kmh, na.rm = TRUE), x = 0, xend = max(a$Cumulative.Distance.m, na.rm = T), color = "red") +
+  xlab("Distance (m)") +
+  ylab("Speed (km/h)") +
+  theme_bw()
+
 ggplot(a, aes(x = Speed.kmh)) +
   geom_histogram(binwidth = 5) +
-  annotate("segment", x = median(a$Speed.kmh, na.rm = TRUE), xend = median(a$Speed.kmh, na.rm = TRUE), y = 0, yend = 1000, color = "red") + 
+  annotate("segment", x = mean(a$Speed.kmh, na.rm = TRUE), xend = mean(a$Speed.kmh, na.rm = TRUE), y = 0, yend = 1000, color = "red") + 
   xlab("Speed (km/h)") + 
   ylab("Number of points") +
   theme_bw()
