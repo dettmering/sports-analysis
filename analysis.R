380ed5561d2cb409a5ec890e2de9f6a60b9af2b5
Till Dettmering <prinzipiell@gmail.com>
2014-02-16 10:42:57 +0100

    Added files

---
+++ analysis.R
@@ -0,0 +1,58 @@
+# Analysis pipeline for my mountainbiking data
+# Read csv from Google MyTracks into 'a' variable, skip first 3 lines
+# e.g., a <- read.csv("~/Dropbox/Matsch.csv", skip = 3)
+
+# Rename columns
+
+colnames(a) <- c('Segment', 'Point', 'Lat', 'Lon', 'Elevation', 'Heading', 'Precision.m', 'Speed.m.s', 'Time', 'Power.W', 'Cadence.rpm')
+
+# Conversion geodetic to cartesian
+# Source DOI: 10.1371/journal.pcbi.1003446
+
+convertCartesian <- function(lat, lon, lat0, lon0) {
+  lat <- lat * pi / 180
+  lon <- lon * pi / 180
+  lat0 <- lat0 * pi / 180
+  lon0 <- lon0 * pi / 180
+  
+  R <- 6378137.0 # Radius of earth (m)
+  f <- 1 / 298.257223563 # Flatness of earth
+  
+  C <- sqrt(1 - (f * (2 - f) * (sin(lat0))^2))
+  R1 <- R / C
+  R2 <- R * (1 - f * (2 - f)) / C^3
+  
+  x <- (lon - lon0) * cos(lat0) * R1
+  y <- (lat - lat0) * R2
+
+  return(c(x, y))
+}
+
+# Calculate Euclidean distance
+
+euclidDist <- function(px, py, qx, qy) {
+  distance <- sqrt((px - qx)^2 + (py - qy)^2)
+  
+  return(distance)
+}
+
+# Convert geodetic to cartesian coordinates
+
+x <- 0
+
+for (x in 1:length(a$Lat)) {
+  a[x, 'x'] <- convertCartesian(a[x, 'Lat'], a[x, 'Lon'], a[1, 'Lat'], a[1, 'Lon'])[1]
+  a[x, 'y'] <- convertCartesian(a[x, 'Lat'], a[x, 'Lon'], a[1, 'Lat'], a[1, 'Lon'])[2]
+}
+
+# Calculate distance
+
+x <- 0
+
+for (x in 1:length(a$x)) {
+  a[x, 'Distance.m'] <- euclidDist(a[x,'x'], a[x,'y'], a[(x + 1),'x'], a[(x + 1),'y'])
+}
+
+# Convert speed to km/h
+
+a$Speed.kmh <- a$Speed.m.s * 3600 / 1000
---
+++ figures.R
@@ -0,0 +1,19 @@
+# Figures
+
+library(ggplot2)
+
+ggplot(a, aes(x = x, y = y)) +
+  geom_point(aes(size = Elevation, color = Speed.kmh)) +
+  xlab("X (m)") +
+  ylab("Y (m)") +
+  scale_color_continuous(name = "Speed (km/h)") +
+  scale_size_continuous(name = "Elevation (m)") +
+  coord_equal() +
+  theme_bw()
+
+ggplot(a, aes(x = Speed.kmh)) +
+  geom_histogram(binwidth = 5) +
+  annotate("segment", x = median(a$Speed.kmh, na.rm = TRUE), xend = median(a$Speed.kmh, na.rm = TRUE), y = 0, yend = 1000, color = "red") + 
+  xlab("Speed (km/h)") + 
+  ylab("Number of points") +
+  theme_bw()
